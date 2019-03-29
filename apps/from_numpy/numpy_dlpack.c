0acb731e0e43d15deee27b66f10e4c5b4e667913
Junru Shao <junrushao1994@gmail.com>
2019-03-28 20:45:12 -0700

    Add an example for borrowing numpy arrays without copying the content (#38)

---
+++ apps/from_numpy/numpy_dlpack.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <dlpack/dlpack.h>
+
+DLManagedTensor given;
+
+void display(DLManagedTensor a) {
+  puts("On C side:");
+  int i;
+  int ndim = a.dl_tensor.ndim;
+  printf("data = %p\n", a.dl_tensor.data);
+  printf("ctx = (device_type = %d, device_id = %d)\n",
+          (int) a.dl_tensor.ctx.device_type,
+          (int) a.dl_tensor.ctx.device_id);
+  printf("dtype = (code = %d, bits = %d, lanes = %d)\n",
+          (int) a.dl_tensor.dtype.code,
+          (int) a.dl_tensor.dtype.bits,
+          (int) a.dl_tensor.dtype.lanes);
+  printf("ndim = %d\n",
+          (int) a.dl_tensor.ndim);
+  printf("shape = (");
+  for (i = 0; i < ndim; ++i) {
+    if (i != 0) {
+      printf(", ");
+    }
+    printf("%d", (int) a.dl_tensor.shape[i]);
+  }
+  printf(")\n");
+  printf("strides = (");
+  for (i = 0; i < ndim; ++i) {
+    if (i != 0) {
+      printf(", ");
+    }
+    printf("%d", (int) a.dl_tensor.strides[i]);
+  }
+  printf(")\n");
+}
+
+void Give(DLManagedTensor dl_managed_tensor) {
+  display(dl_managed_tensor);
+  given = dl_managed_tensor;
+}
+
+void Finalize() {
+  given.deleter(&given);
+}
