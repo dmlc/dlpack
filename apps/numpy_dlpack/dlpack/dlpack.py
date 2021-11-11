a8afc88305b5ef53eb5aec1d53b17f9f702b2cf4
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2021-11-11 15:21:03 -0600

    Added docstrings to classes representing dlpack's structs

--- apps/numpy_dlpack/dlpack/dlpack.py
+++ apps/numpy_dlpack/dlpack/dlpack.py
@@ -4,6 +4,9 @@ _c_str_dltensor = b"dltensor"
 
 
 class DLDeviceType(ctypes.c_int):
+    """The enum that encodes the type of the device where
+    DLTensor memory is allocated.
+    """
     kDLCPU = 1
     kDLCUDA = 2
     kDLCUDAHost = 3
@@ -33,6 +36,11 @@ class DLDeviceType(ctypes.c_int):
 
 
 class DLDevice(ctypes.Structure):
+    """Represents the device where DLTensor memory is allocated.
+    The device is represented by the pair of fields:
+       device_type: DLDeviceType
+       device_id: c_int
+    """
     _fields_ = [
         ("device_type", DLDeviceType),
         ("device_id", ctypes.c_int),
@@ -40,6 +48,7 @@ class DLDevice(ctypes.Structure):
 
 
 class DLDataTypeCode(ctypes.c_uint8):
+    """An integer that encodes the category of DLTensor elements' data type."""
     kDLInt = 0
     kDLUInt = 1
     kDLFloat = 2
@@ -59,6 +68,13 @@ class DLDataTypeCode(ctypes.c_uint8):
 
 
 class DLDataType(ctypes.Structure):
+    """Descriptor of data type for elements of DLTensor.
+    The data type is described by a triple, `DLDataType.type_code`,
+    `DLDataType.bits`, and `DLDataType.lanes`.
+
+    The element is understood as packed `lanes` repetitions of
+    elements from `type_code` data-category of width `bits`.
+    """
     _fields_ = [
         ("type_code", DLDataTypeCode),
         ("bits", ctypes.c_uint8),
@@ -83,6 +99,21 @@ class DLDataType(ctypes.Structure):
 
 
 class DLTensor(ctypes.Structure):
+    """Structure describing strided layout of DLTensor.
+    Fields are:
+       data:  void pointer
+       device: DLDevice
+       ndim: number of indices needed to reference an
+             element of the tensor
+       dtype: data type descriptor
+       shape: tuple with lengths of the corresponding
+              tensor dimensions
+       strides: tuple of numbers of array elements to
+                step in each dimension when traversing
+                the tensor
+       byte_offset: data + byte_offset gives the address of
+                tensor element with index (0,) * ndim
+    """
     _fields_ = [
         ("data", ctypes.c_void_p),
         ("device", DLDevice),
@@ -95,6 +126,10 @@ class DLTensor(ctypes.Structure):
 
 
 class DLManagedTensor(ctypes.Structure):
+    """Structure storing the pointer to the tensor descriptor,
+    deleter callable for the tensor descriptor, and pointer to
+    some additional data. These are stored in fields `dl_tensor`,
+    `deleter`, and `manager_ctx`."""
     _fields_ = [
         ("dl_tensor", DLTensor),
         ("manager_ctx", ctypes.c_void_p),
